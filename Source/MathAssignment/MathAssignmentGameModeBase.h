f652bacfa37b094bbcfe16cff81d0668deb42a50
MartinCarlsson112 <Martin.carlsson@futuregames.nu>
2021-01-21 13:14:34 +0100

    Upload clean project

---
+++ Source/MathAssignment/MathAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MathAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATHASSIGNMENT_API AMathAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
